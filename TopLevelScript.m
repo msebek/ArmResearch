b2ca24dd48f98e0c21ab3654f7275050bafd22d0
Matt Sebek <msebek@andrew.cmu.edu>
2015-04-09 11:28:56 -0400

    Added pdf things

--- TopLevelScript.m
+++ TopLevelScript.m
@@ -1,5 +1,5 @@
 % Load all the data files. 
-filename = 'arm_april_data_first_set';
+filename = 'arm_april_data_second_set';
 load(filename, 'aprilposes', 'armposes');
 
 % Process them. 
@@ -11,6 +11,18 @@ load(filename, 'aprilposes', 'armposes');
 % 3. Create a KDtree of the april poses to actual position. 
 % All observed april poses; rows are observations
 % These are the keys (as strings)
+
+%first = aprilposes
+%second = 
+
+% Think about different averaging functions
+
 knn_search_object = createns(aprilposes', 'Distance', 'euclidean', ...
         'NSMethod', 'kdtree');
 
+arm_estimate = interpolate_measurement(knn_search_object, ...
+        aprilposes, armposes, 3, aprilposes(:, 2));
+    
+    
+    
+arm_estimate;
--- interpolate_measurement.m
+++ interpolate_measurement.m
@@ -1,5 +1,6 @@
 function [arm_estimate] = interpolate_measurement(knn_search_object, ...
-    associated_arm_poses, num_nearest_neighbors, april_measurement)
+    april_poses, associated_arm_poses, ...
+    num_nearest_neighbors, april_measurement)
 % Returns the estimated arm position from a given april_measurement
 
     % Lookup a thing, and do interpolation
@@ -7,21 +8,41 @@ function [arm_estimate] = interpolate_measurement(knn_search_object, ...
         num_nearest_neighbors);
 
     k_arm_poses = associated_arm_poses(:, idx);
-
+    k_april_poses = april_poses(:, idx);
+    
     % Use weighted average
     % Make zeros just barely non-zero
     dist_equal_zero = (dist == 0);
     dist(dist_equal_zero) = 0.000001;
     
-    %
-    k_arm_pose_weights = 1 ./ dist;
+    % Simple Averaging
+    %k_arm_pose_weights = 1 ./ dist;
+    %k_arm_pose_weights = k_arm_pose_weights / sum(k_arm_pose_weights);
+    %k_weighted_arm_poses = zeros(7,1);
+    %for i = 1:size(k_arm_pose_weights, 2)
+    %   k_weighted_arm_poses = k_weighted_arm_poses + ...
+    %            k_arm_poses(:, i) * k_arm_pose_weights(i); 
+    %end
+    
+    % Intelligent averaging using Multivariate normal probability
+    %  density. 
+    % mvnpdf: Multivariate normal probability density function
+    %  y = mvnpdf(X, {mu, {sigma}})
+    %   X = nx1 vector, containing the PDF of the multivariate
+    %       normal distribution with zero mean and identity 
+    %       covariance matrix. 
+    %  (TODO the covariance can be tuned to get better results.)
+    mean = april_measurement(4:9, :)'
+    cov = eye(6) / 5000;
+    measurements = k_april_poses(4:9, :)'
+    k_arm_pose_weights = mvnpdf(measurements, mean, cov)';
     k_arm_pose_weights = k_arm_pose_weights / sum(k_arm_pose_weights);
     k_weighted_arm_poses = zeros(7,1);
     for i = 1:size(k_arm_pose_weights, 2)
        k_weighted_arm_poses = k_weighted_arm_poses + ...
                 k_arm_poses(:, i) * k_arm_pose_weights(i); 
     end
-
+    
     % Put together weighted averages for the full estimate
     arm_estimate = k_weighted_arm_poses;
 
